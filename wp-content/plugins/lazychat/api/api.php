1e69aa4aef54d1da55053601bfb10ba54f584500
Sayedur Rahman <s.rahman3443@gmail.com>
2023-01-04 12:55:58 +0600

    removed test function

--- wp-content/plugins/lazychat/api/api.php
+++ wp-content/plugins/lazychat/api/api.php
@@ -1288,59 +1288,6 @@ class Lswp_api extends WP_REST_Controller
 
 		return $variation;
 	}
-
-	public function lcwp_test()
-	{
-		$product = new WC_Product_Variable();
-		$product->set_description('T-shirt variable description');
-		$product->set_name('T-shirt variable');
-		$product->set_sku('test-shirt');
-		$product->set_price(1);
-		$product->set_regular_price(1);
-		$product->set_stock_status();
-		$product->save();
-
-		$atts = [];
-		$atts[] = $this->pricode_create_attributes('color', ['red', 'green']);
-		$atts[] = $this->pricode_create_attributes('size', ['S', 'M']);
-
-		//Adding attributes to the created product
-		$product->set_attributes($atts);
-		$product->save();
-
-		$data = new stdClass();
-		$data->sku = 'sku-123';
-		$data->price = '10';
-		$this->pricode_create_variations($product->get_id(), ['color' => 'red', 'size' => 'M'], $data);
-		return $product;
-	}
-
-	public function pricode_create_attributes($name, $options)
-	{
-		$attribute = new WC_Product_Attribute();
-		$attribute->set_id(0);
-		$attribute->set_name($name);
-		$attribute->set_options($options);
-		$attribute->set_visible(true);
-		$attribute->set_variation(true);
-		return $attribute;
-	}
-
-	function pricode_create_variations($product_id, $values, $data)
-	{
-		$variation = new WC_Product_Variation();
-		$variation->set_parent_id($product_id);
-		$variation->set_attributes($values);
-		$variation->set_status('publish');
-		$variation->set_sku($data->sku);
-		$variation->set_price($data->price);
-		$variation->set_regular_price($data->price);
-		$variation->set_stock_status();
-		$variation->save();
-		$product = wc_get_product($product_id);
-		$product->save();
-		return $product;
-	}
 }
 
 function init_rest_api_endpoint()
